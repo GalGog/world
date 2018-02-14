3382d08d98d3e8f45532c185ae99714c82f1f08b
GalGog <galynna00@gmail.com>
2018-02-14 12:18:34 +0000

    testing angular

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,105 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `    
+  template: ` 
+  <div *ngFor="let color of colors; first as f">
+    <h2>{{f}}{{color}}</h2>
     
-  <h2>Welcome  {{name}}</h2>
-  <button (click)="onClick($event)">Greet</button>
-  <button (click)="greeting = 'welcom wishas' ">Greet</button>
+  </div>
   
-  
-  
-  
-  <h2 [style.color]="'orange'">Style code</h2>
-  <h2 [style.color]="highlightColor">Style Binging</h2>
-  <h2 [ngStyle]="titleStyle">Style Binging 3 </h2>
-  
-  
-  <h2 class="text-success">Codeevolution</h2>
-  <h2 [class] = "successClass">Codeevolution</h2>
-  <h2 [class.text-danger] = "hasError">Codeevolution</h2>
-  
-
-  <input [id]="myId" type="text" value="name">
-  <input [disabled] = "isDisabled" id = {{myId}} type="text" value="name">
-
-  <h3> lesson 10
-  <input #myInput type="text">
-  <button (click) = "logMessage(myInput.value)">Log</button>
-  </h3>
-
-
-  <h2>{{45+2}}</h2>
-    <h2>weclome + {{name}}</h2>
-    <h2>{{name.length}}</h2>
-    <h2>{{name.toUpperCase()}}</h2>
-    <h2>{{greetUser()}}</h2>
-    <h2>{{siteUrl}}</h2>
-    
-    
-    <input  [(ngModel)] = "age" type="text">
-    {{age}}
-    
-    <h2 *ngIf="displayName ; else elseBlock">
-      Lesson 12 Codeevolution 
-    </h2>
-  
-    <ng-template #elseBlock> 
-    <h4>
-      Name is hidden
-    </h4>
-    </ng-template>
-    
-    
-    `,
-  styles: [`
-  .text-success{
-    color: green;
-  }
-    .text-danger{
-      color: red;
-    }
-    .text-special{
-      font-style: normal;
-    }
-  `]
+  `,
+  styles: []
 })
 export class TestComponent implements OnInit {
+ public colors = ["red", "blue", "green", "yellow"];
 
-public  name  = 'Odo';
-public myId = "testId";
-public  siteUrl = window.location.href;
-public isDisabled = false;
-public  successClass = "text-success";
-public hasError = false;
-public isSpecial = true;
-public highlightColor = "orange";
-public titleStyle = {
-  color: "blue",
-  fondStyle: "italic",
-}
-public greeting = "";
-
-public age = "lesson11";
-
-
-displayName = true;
   constructor() {
-    console.log('name');
-    console.log(name);
   }
 
   ngOnInit() {
-  }
-  greetUser(){
-    return "Hello" + this.name;
-  }
-  onClick(event){
-    console.log(event);
-    this.greeting = event.type;
-  }
-
-  logMessage (value){
-    console.log(value);
   }
 }
