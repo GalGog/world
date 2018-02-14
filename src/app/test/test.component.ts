c857864fad2e67dfac6b5aa8bfe4f4d8c812426f
GalGog <galynna00@gmail.com>
2018-02-14 14:34:13 +0000

    testing angular

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,12 +4,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   selector: 'app-test',
   template: ` 
     
-    <h2>{{"Hello" + name}}</h2>
-    <button (click)="fireEvent()" )>Send Event</button>
-    
-  <div *ngFor="let color of colors; even as e">
-    <h2>{{e}} {{color}}</h2>
-  </div>
+    <h2>{{name}}</h2>
   
   `,
   styles: []
