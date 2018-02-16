de196f4c68d7676def5da611d7918b8a67c29ced
GalGog <galynna00@gmail.com>
2018-02-16 15:26:09 +0000

    testing angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {FormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
     AppComponent,
     TestComponent,
     EmployeeListComponent,
+    EmployeeDetailComponent,
   ],
   imports: [
     BrowserModule,
