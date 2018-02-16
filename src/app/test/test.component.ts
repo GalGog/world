81e887eb48e4448832f6da47402cf67a431723a9
GalGog <galynna00@gmail.com>
2018-02-16 10:23:00 +0000

    testing angular

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
     <h2>{{name | lowercase}}</h2>
     <h2>{{name | uppercase}}</h2>
     <h2>{{name | titlecase}}</h2>
-    <h2>{{name | slice: 2:10}}</h2>
+    <h2>{{name | slice: 2:6}}</h2>
+    <h2>{{person | json}}</h2>
+    <h2>{{name | json}}</h2>
+    
+    <h2>{{5.678 | number:'1.2-3'}}</h2>
+    <h2>{{5.678 | number:'3.4-5'}}</h2>
+    <h2>{{5.678 | number:'3.1-2'}}</h2>
+
+    <h2>{{0.23 | percent}}</h2>
+    <h2>{{0.25| currency}}</h2>
+    <h2>{{0.25| currency: 'GBP'}}</h2>
+    <h2>{{0.25| currency: 'EUR': 'code'}}</h2>
+
+    <h2>{{date}}</h2>
+    <h2>{{date | date:'short'}}</h2>
+    <h2>{{date | date:'shortDate'}}</h2>
+    <h2>{{date | date:'shortTime'}}</h2>
+
+
+
+
 
 
 
@@ -17,12 +37,13 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class TestComponent implements OnInit {
- public  name = "Having 1 years of experience as a JavaScript Developer so far, I build websites and applications based on web standards, cross browser support, responsive design and clean code. ";
+ public  name = "Codeevolution ";
  public message = "Welcom to Codeevolution";
  public  person = {
    "firstName": "Jon",
    "lastName": "Doe"
  }
+ public date = new Date();
   constructor() {
   }
 
